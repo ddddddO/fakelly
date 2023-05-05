b68b057e956df44b0ed1d2d5510a23cd4a4441af
ddddddO <lbfdeatq@gmail.com>
2023-05-05 11:21:14 +0900

    add todo

--- app/Main.hs
+++ app/Main.hs
@@ -10,6 +10,8 @@ command :: String -> [String] -> IO ()
 command "ls" = ls'
 command "tree" = tree'
 command "wc" = wc
+-- TODO: 以下で、入力をパースして何かできるようにしたい。例えば、「stack exec fakelly 'ls . | wc'」が評価できるような。
+--       インタプリタ作るような？ https://github.com/ddddddO/monkey たぶんこの本が役立ちそう
 command xxx = \_ -> putStrLn $ "'" ++ xxx ++ "'" ++ " is not implemented."
 
 main :: IO ()
