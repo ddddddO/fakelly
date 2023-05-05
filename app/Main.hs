1aef27e41b5e4d82287d5e765da01d7f048e261d
ddddddO <lbfdeatq@gmail.com>
2023-05-05 11:33:25 +0900

    nits

--- app/Main.hs
+++ app/Main.hs
@@ -6,15 +6,15 @@ import Ls
 import Tree
 import Wc
 
-command :: String -> [String] -> IO ()
-command "ls" = ls'
-command "tree" = tree'
-command "wc" = wc
+dispatch :: String -> [String] -> IO ()
+dispatch "ls" = ls'
+dispatch "tree" = tree'
+dispatch "wc" = wc
 -- TODO: 以下で、入力をパースして何かできるようにしたい。例えば、「stack exec fakelly 'ls . | wc'」が評価できるような。
 --       インタプリタ作るような？ https://github.com/ddddddO/monkey たぶんこの本が役立ちそう
-command xxx = \_ -> putStrLn $ "'" ++ xxx ++ "'" ++ " is not implemented."
+dispatch xxx = \_ -> putStrLn $ "'" ++ xxx ++ "'" ++ " is not implemented."
 
 main :: IO ()
 main = do
   (cmd:args) <- getArgs
-  command cmd args
+  dispatch cmd args
