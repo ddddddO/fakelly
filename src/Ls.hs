eb2e2e86924417eddcc5db6bb7fba2191a6ed7a0
ddddddO <lbfdeatq@gmail.com>
2023-06-03 19:47:19 +0900

    ls 複数ディレクトリ指定

--- src/Ls.hs
+++ src/Ls.hs
@@ -7,9 +7,14 @@ import           System.Directory
 
 -- fakelly ls
 -- fakelly ls .
+-- fakelly ls src test app
 ls' :: [String] -> IO ()
 ls' []               = ls "." >>= mapM_ putStrLn
-ls' (path:remaining) = ls path >>= mapM_ putStrLn -- TODO: 引数に指定した複数のパスをlsするのもいいかも
+ls' (path:remaining) = do
+  ls path >>= mapM_ putStrLn
+  case remaining of
+    [] -> return ()
+    otherwise -> ls' remaining
 
 ls :: String -> IO [String]
 ls dir = getDirectoryContents dir
